4f9fdaba517b8ab847ca5ddf58dd8091357e89ab
Tyler <tylercweddle@gmail.com>
2018-02-07 18:36:08 -0500

    Git Branch Test

---
+++ Calculator/CalculatorModel.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorModel.swift
+//  Calculator
+//
+//  Created by Tyler Christopher Weddle on 2/7/18.
+//  Copyright © 2018 Tyler Weddle. All rights reserved.
+//
+
+import Foundation
